e90dfe2e0b0bf874139526daf1d9aff0bf51ecf2
moritzuehling <github@moritzuehling.de>
2024-03-25 11:46:49 +0100

    update

---
+++ .scripts/modmic.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+pactl set-default-source alsa_input.usb-Antlion_Audio_Antlion_Wireless_Microphone-00.mono-fallback
+notify-send "MidMic"
---
+++ .scripts/wired.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+pactl set-default-source alsa_input.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.analog-mono
+notify-send "Wired Mic"
