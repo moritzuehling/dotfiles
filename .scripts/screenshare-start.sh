5c5ea3717c6a411b8dd29e5c893536a88f22477f
moritzuehling <github@moritzuehling.de>
2024-04-01 22:37:04 +0200

    update

--- .scripts/screenshare-start.sh
+++ .scripts/screenshare-start.sh
@@ -3,6 +3,6 @@
 swaync-client --inhibitor-add "xdg-desktop-portal-wlr"
 ln -sf $HOME/.config/hypr/screenshare-active.conf $HOME/.config/hypr/screenshare-link.conf
 ln -sf $HOME/.config/waybar/screnshare.css $HOME/.config/waybar/active.css
-killall -SIGUSR2 waybar
+touch -m $HOME/.config/waybar/style.css
 
 swww img "$HOME/Pictures/wallpapers/screenshare.png" --transition-step 255
--- .scripts/screenshare-stop.sh
+++ .scripts/screenshare-stop.sh
@@ -3,6 +3,7 @@
 swaync-client --inhibitor-remove "xdg-desktop-portal-wlr"
 ln -sf $HOME/.config/hypr/screenshare-inactive.conf $HOME/.config/hypr/screenshare-link.conf
 ln -sf $HOME/.config/waybar/no-screenshare.css $HOME/.config/waybar/active.css
-killall -SIGUSR2 waybar
+
+touch -m $HOME/.config/waybar/style.css
 
 swww img "$HOME/Pictures/wallpapers/1.png" --transition-step 255
