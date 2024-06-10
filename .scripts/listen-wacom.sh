9a7cd0a2fc60f8ac32694a542cc5bf9cd1598c59
moritzuehling <github@moritzuehling.de>
2024-06-10 12:11:43 +0200

    automatically find right workspace lol

--- .scripts/listen-wacom.sh
+++ .scripts/listen-wacom.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 DEVICE=/dev/input/by-id/usb-Wacom_Co._Ltd._Intuos_BT_M_2EH00U2013893-event-mouse
-WORKSPACE=7
 
-evtest $DEVICE | grep --line-buffered "type 1 (EV_KEY), code 330 (BTN_TOUCH), value 1" | while read -r LINE; do echo $LINE; hyprctl dispatch workspace $WORKSPACE; done
+evtest $DEVICE |\
+	grep --line-buffered "type 1 (EV_KEY), code 330 (BTN_TOUCH), value 1" |\
+	while read -r LINE; do \
+	echo $LINE;\
+	WORKSPACE=$(hyprctl clients -j | jq '.[] | select(.class=="com.github.xournalpp.xournalpp") | .workspace.id');\
+	echo $WORKSPACE;\
+	hyprctl dispatch workspace $WORKSPACE;
+	FOCUS=$(hyprctl clients -j | jq '.[] | select(.class=="com.github.xournalpp.xournalpp") | .focusHistoryID');\
+	if [ $FOCUS -ne 0 ]; then\
+		hyprctl dispatch focuswindow class:com.github.xournalpp.xournalpp;\
+		echo "focused";\
+	fi
+done
+
+# hyprctl clients -j | jq '.[] | select(.class=="com.github.xournalpp.xournalpp") |
+# 	hyprctl dispatch focuswindow class:com.github.xournalpp.xournalpp;\
+
 
 
