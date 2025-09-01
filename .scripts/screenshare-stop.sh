ea5f680314d13e6fd98f1fd1315aeb6e948ce522
moritzuehling <github@moritzuehling.de>
2025-09-01 14:34:58 +0200

    config updates

--- .scripts/screenshare-stop.sh
+++ .scripts/screenshare-stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-swaync-client --inhibitor-remove "xdg-desktop-portal-wlr"
+swaync-client --inhibitor-remove "xdg-desktop-portal-hyprland"
 ln -sf $HOME/.config/hypr/screenshare-inactive.conf $HOME/.config/hypr/screenshare-link.conf
 ln -sf $HOME/.config/waybar/no-screenshare.css $HOME/.config/waybar/active.css
 
